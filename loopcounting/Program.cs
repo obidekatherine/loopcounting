6c8b42f3559ff74f0aa34aa42d81d241438b02d4
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2021-02-26 05:18:49 +0100

    Add project files.

---
+++ loopcounting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace loopcounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x < 10; x++)
+            {
+                for (int i = 1; i <= 20; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                for (int i = 1; i >= 20; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            
+     
+        }
+    }
+}
